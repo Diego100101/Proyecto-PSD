1cc9d1c20b3f7b2d6ed58f4cfb6db9296c20b9d4
Diego100101 <81589753+Diego100101@users.noreply.github.com>
2022-12-11 18:52:18 -0600

    Update Proyecto.m

--- Proyecto.m
+++ Proyecto.m
@@ -8,12 +8,24 @@
 %Diego Cantoral González
 %Emanuel Martínez Felipe
 %---------------------------------------------------------------------------
+%Información e importación de la señal
+%---------------------------------------------------------------------------
 [s,fs]=audioread('Domini_Fil.wav');
-%Información de la señal:
 %    Canales = 1
-%    fs = 8000 frecuencia de muestreo
+%    fs = 8000 = 8ksps frecuencia de muestreo
 %    N = 88576 muestras totales
 %    Duración = 11.072
 %    Bits por muesta = 16
 %    BitRate = -1
+%---------------------------------------------------------------------------
+%Definición de filtro ideal
+%---------------------------------------------------------------------------
+%Realización del filtro pasa bajos
+M = 88576; %muestras para el filtro ideal
+Bw = 250*2; %ancho de banda
+unos = floor( M * Bw ) %definición de los unos
+if mod( unos, 2 ) == 0
+  filtro += 1;
+end
+ceros = M - unos;
 plot(s)
