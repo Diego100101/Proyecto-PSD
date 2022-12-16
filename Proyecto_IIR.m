921712e7deb3714eac17c46c54019708a042b735
Diego100101 <81589753+Diego100101@users.noreply.github.com>
2022-12-15 21:48:16 -0600

    Update Proyecto_IIR.m

--- Proyecto_IIR.m
+++ Proyecto_IIR.m
@@ -17,21 +17,34 @@ clear;
 duracion = length(s)/fs;
 
 fc = [500/fs, 1000/fs]; % Frecuencias de paso normalizadas
-orden = 5;
+orden = 5; 
 
-[bCoef, aCoef] = butter(orden, fc, 'bandpass');
+ % Función butter, recibe orden y vector con las frecuencias de paso
+[bCoef, aCoef] = butter(orden, fc, 'bandpass'); 
 
-[h, w] = freqz(bCoef, aCoef); % Respuesta en frecuencia del filtro
+% Respuesta en frecuencia del filtro
+[h, w] = freqz(bCoef, aCoef); 
 
-S = abs(fftshift(fft(s)));
+% Espectro de la señal original
+S = abs(fftshift(fft(s))); 
 
+% Filtración de la señal
 senalFiltrada = filter(bCoef, aCoef, s);
 
+% Espectro de la señal filtrada
 senalFiltradaEspectro = fftshift(fft(senalFiltrada));
+
+%% Graficación
+
+% Respuesta en frecuencia del filtro pasa banda IIR
+figure();
+plot(w, abs(h), 'r')
+
+% Espectro de la señal filtrada
 figure();
 plot(abs(senalFiltradaEspectro))
-% plot(w, abs(h), 'r')
 
+% Sonido de la señal filtrada
 sound(senalFiltrada,fs)
 pause(duracion + 1);
 
